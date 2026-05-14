e75131137b7c29e9d853acc21ec90d982c3e3eed
LucasVinnic <lucasvinnic26@gmail.com>
2026-05-14 14:20:52 -0300

    Criando models

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+class Contact(models.Model):
+    name = models.CharField(max_length=50, blank=True)
+    aka = models.CharField(max_length=50, blank=True)
+    phone = models.CharField(max_length=50, blank=True)
+    email = models.EmailField(max_length=50, blank=True)
+    created_date = models.DateTimeField(default=timezone.now)
+    description = models.TextField(blank=True)
